a85c5d257ad09efa275dbbd87308f9b2dd107670
bert <49372063+bertman80@users.noreply.github.com>
2021-12-26 05:56:24 +0100

    Update zabbix.sql

--- zabbix.sql
+++ zabbix.sql
@@ -1,4 +1,3 @@
-mysql -uroot -p
 create database zabbix character set utf8 collate utf8_bin;
 create user zabbix@localhost identified by 'PASSWORD';
 grant all privileges on zabbix.* to zabbix@localhost;
