6b5ecadf29b6529e079fc49b5733196a44253647
bert <49372063+bertman80@users.noreply.github.com>
2022-02-08 15:24:06 +0100

    Update and rename lamp-server.txt to lamp-server.sh

--- lamp-server.sh
+++ lamp-server.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 #curl -L https://raw.githubusercontent.com/bertman80/linux-basic-setup/main/lamp-server.txt | bash
 echo "### Apache WebServer Installeren ###"
 
-sudo apt-get install apache2 php mariadb-server php-mysql -y
-sudo service apache2 restart
-"<?php phpinfo(); ?>" > /var/www/html/info.php
+#sudo apt-get install apache2 php mariadb-server php-mysql -y
+#sudo service apache2 restart
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 
 externalip=curl ifconfig.me
 internalip=hostname -I | awk '{print $1}'
