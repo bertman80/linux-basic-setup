ecc8ef72a9a46a5751fce09d1741a92d29cc5e33
bert <49372063+bertman80@users.noreply.github.com>
2022-02-09 06:36:11 +0100

    Update lamp-server.sh

--- lamp-server.sh
+++ lamp-server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 #curl -L https://raw.githubusercontent.com/bertman80/linux-basic-setup/main/lamp-server.txt | bash
+internsubnet=192.168.178.0/24
+wordpress_url="https://nl.wordpress.org/latest-nl_NL.zip"
+
 echo "### Apache WebServer Installeren ###"
 
 sudo apt-get install apache2 php mariadb-server php-mysql -y
@@ -11,10 +14,16 @@ internalip=$(hostname -I | awk '{print $1}')
 echo "start de webbrowser"
 echo "http://$internalip/info.php"
 
+curl -o /var/www/html/ $wordpress_url
+
 echo "### FireWall ###" 
 sudo apt install ufw
 # ssh alleen van intern
-ufw allow from 192.168.178.0/24 to any app ssh
+#ufw allow from $internsubnet to any app ssh
 # website overal te benaderen
-ufw allow from any to any app www
+ufw allow from $internsubnet to any app www
+#ufw allow from any to any app www
 sudo ufw enable
+# show rules
+sudo ufw status numbered
+
